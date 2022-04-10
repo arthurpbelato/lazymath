234cfb7aebb9114a82646868d9a69c2f76af9ac7
Arthur Poubel Belato <belatoarthur@outlook.com>
2022-04-10 10:57:02 -0300

    adding swagger dependency

--- service/src/main/java/io/arhturpbelato/service/resource/PythagoreanTheoremResource.java
+++ service/src/main/java/io/arhturpbelato/service/resource/PythagoreanTheoremResource.java
@@ -2,6 +2,8 @@ package io.arhturpbelato.service.resource;
 
 import io.arhturpbelato.service.model.PythagoreanTheorem;
 import io.arhturpbelato.service.service.PythagoreanTheoremService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/pythagorean-theorem")
+@Api(value = "API REST Pythagorean Theorem", tags = "Pythagorean Theorem")
 public class PythagoreanTheoremResource {
 
     private final PythagoreanTheoremService service;
 
     @PostMapping
+    @ApiOperation(value = "Solve pythagorean theorem.")
     @SneakyThrows
     public ResponseEntity<PythagoreanTheorem> solve(@RequestBody PythagoreanTheorem theorem) {
         service.solve(theorem);
